3acd01211349949bc28b96e64a1a892c98236101
ja3600 <pyman2112@outlook.com>
2026-09-20 23:40:01 +0000

    create default gui admin account

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -32,3 +32,4 @@ fi
 
 cd netbox
 python manage.py migrate --noinput
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', password='admin')"
